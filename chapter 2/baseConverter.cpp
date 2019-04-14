0d680ac4906833877210d0f3a8cc2f083bc8081d
Eliram <eliram13@gmail.com>
2019-04-14 22:22:36 +0900

    finished work on base converter for bases 2-16

--- chapter 2/baseConverter.cpp
+++ chapter 2/baseConverter.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 
 using std::cin;
 using std::cout;
@@ -8,21 +9,21 @@ enum modes {DECIMAL, BINARY, HEXADECIMAL}
 inputMode, outputMode;
 
 int DigitHexaToDecimal(const char c) {
-	if (c <= '9') {
+	if (c <= '9' && c >= '0') {
 		return  (c - '0');
 	}
 	return 10 + c - 'a';
 }
 
 char DigitDecimalToHexa(const int decimal) {
-	//assert decimal >= 0 && dcimal <= 15
-	const int table[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+	//assert decimal >= 0 && decimal <= 15
 	if (decimal <= 9) return '0' + decimal;
+	const int table[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
 	return table[decimal % 10];
 }
 
 int HexaToDecimal() {
-	 int buffer = 0;
+	int buffer = 0;
 	char hexaDigit = cin.get();
 	while (hexaDigit != '\n') {
 		buffer = buffer * 16 + DigitHexaToDecimal(hexaDigit);
@@ -52,7 +53,7 @@ void DecimalToHexa(int decimalNumber) {
 }
 
 int binaryToDecimal() {
-	unsigned long int DecimalNumber = 0;
+	int DecimalNumber = 0;
 	char c;
 	
 	c = cin.get();
@@ -65,11 +66,108 @@ int binaryToDecimal() {
 	return DecimalNumber;
 }
 
+void decimalToBinary(int decimalNumber) {
+	//assert decimal number is positive
+	std::string buffer = ""; 
+	int reminder;
+	int factor;
+	do {
+		factor = decimalNumber / 2;
+		reminder = decimalNumber % 2;
+		buffer += DigitDecimalToHexa(reminder);
+		decimalNumber = factor;
+	} while (decimalNumber != 0);
+
+	for (std::string::reverse_iterator ri = buffer.rbegin(); ri != buffer.rend(); ++ri) {
+		cout << *ri;
+	}
+	cout << '\n';
+	return;
+}
+
+void setInputMode() {
+	char input;
+	do {
+		cout << "enter input base: (h)exa, (d)ecimal (b)inary " << endl;
+		cin >> input;
+		cin.ignore();
+		switch (input) {
+		case 'd':
+			inputMode = DECIMAL;
+			break;
+		case 'b':
+			inputMode = BINARY;
+			break;
+		case 'h':
+			inputMode = HEXADECIMAL;
+			break;
+		default:
+			cout << "unrecognized charchter entered " << endl;
+		}
+	} while (input != 'd' && input != 'h' && input != 'b');
+}
+
+void setOutputMode() {
+	char output;
+	do {
+		cout << "enter output base: (h)exa, (d)ecimal (b)inary " << endl;
+		cin >> output;
+		cin.ignore();
+		switch (output) {
+		case 'd':
+			outputMode = DECIMAL;
+			break;
+		case 'b':
+			outputMode = BINARY;
+			break;
+		case 'h':
+			outputMode = HEXADECIMAL;
+			break;
+		default:
+			cout << "unrecognized charchter entered " << endl;
+		}
+	} while (output != 'd' && output != 'h' && output != 'b');
+}
+
+
 
 int main() {
-	int decimal;
-	cin >> decimal;
-	DecimalToHexa(decimal);
+	setInputMode();
+	setOutputMode();
 
+	cout << "entere value to be converted: " << endl;
+	
+	//assert inputed apropriate value
+	int decimalValue;
+	switch (inputMode){
+		case DECIMAL:
+			cin >> decimalValue;
+			break;
+		case BINARY:
+			decimalValue = binaryToDecimal();
+			break;
+		case HEXADECIMAL:
+			decimalValue = HexaToDecimal();
+			break;
+		default: 
+			cout << "an error has ocurred " << endl;
+	}
+
+	switch (outputMode){
+		case DECIMAL:
+			cout << "the Decimal value is " << decimalValue << endl;
+			break;
+		case BINARY:
+			cout << "The binary value is: ";
+			decimalToBinary(decimalValue);
+			break;
+		case HEXADECIMAL:
+			cout << "The hexa decimal value is: ";
+			DecimalToHexa(decimalValue);
+			break;
+		default:
+			cout << "an error has ocurred " << endl;
+	}
+	
 }
 
---
+++ chapter 2/baseConverter2.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using integer = unsigned int;
+
+integer inputMode =16, outputMode = 16;
+
+char decimaltoChar(const integer decimalValue) {
+	//assert decimal >= 0 && decimal < inputmode
+	if (decimalValue <= 9) return '0' + decimalValue;
+    return 'a' + decimalValue - 10;
+}
+
+integer charToDecimal(const char digit) {
+	if (digit <= '9' && digit >= '0') {
+		return  (digit - '0');
+	}
+	return 10 + digit - 'a';
+}
+
+integer toDecimal(std::string string) {
+	//assert charchters in string are between in the range of inputMode
+	integer DecimalNumber = 0;
+	for(std::string::iterator it = string.begin(); it != string.end(); ++it){
+
+		DecimalNumber *= inputMode;
+		DecimalNumber += charToDecimal(*it);
+	}
+	return DecimalNumber;
+}
+
+std::string fromDecimal(integer decimalValue) {
+	//assert decimal number is positive
+	std::string buffer = "";
+	
+	integer reminder;
+	integer factor;
+	do {
+		factor = decimalValue / outputMode;
+		reminder = decimalValue % outputMode;
+		buffer += decimaltoChar(reminder);
+		decimalValue = factor;
+	} while (decimalValue != 0);
+
+	std::string newValue = "";
+	for (std::string::reverse_iterator ri = buffer.rbegin(); ri != buffer.rend(); ++ri) {
+		newValue += *ri;
+	}
+	return newValue;
+}
+
+int main() {
+	do {
+		cout << "Enter input base from 2 to 16 " << endl;
+		cin >> inputMode;
+	} while (inputMode < 2 || inputMode > 16);
+
+	do {
+		cout << "Enter output base from 2 to 16 " << endl;
+		cin >> outputMode;
+	} while (outputMode < 2 || outputMode > 16);
+	
+	cout << "enter value in base " << inputMode << " to be converted to base " << outputMode << endl;
+	std::string str;
+	cin >> str;
+	
+	cout << fromDecimal(toDecimal(str));
+}
