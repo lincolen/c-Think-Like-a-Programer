3c59d38d2fabca3fbfd1996665fc2b2e047e7285
Eliram <eliram13@gmail.com>
2019-05-14 13:50:54 +0900

    finished extended int add

--- chapter 4 pointers/integerAsList.cpp
+++ chapter 4 pointers/integerAsList.cpp
@@ -5,112 +5,123 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-struct intNode {
-	int digit;
-	intNode * next;
-};
+	struct intNode {
+		int digit;
+		intNode * next;
+	};
 
-using extendedInt = intNode * ;
+	//alias for new int type
+	using extendedInt = intNode * ;
 
 
-//least significant it first
-extendedInt intToList(int input){
-	
-	extendedInt newInt = nullptr;
-	intNode * node;
+	//least significant digit first
+	extendedInt intToList(int input) {
 
-	if (input < 0) {
-		std::cerr << "input is negetive" << endl;
-		return newInt;
-	}
+		extendedInt newInt = nullptr;
+		intNode * node;
 
-	node = new intNode;
-	newInt = node;
-	node->digit = input % 10;
-	input /= 10;
-	while (input != 0) {
-		node->next = new intNode;
-		node = node->next;
+		if (input < 0) {
+			std::cerr << "input is negetive" << endl;
+			return newInt;
+		}
+
+		node = new intNode;
+		newInt = node;
 		node->digit = input % 10;
 		input /= 10;
-	}
-	node->next = nullptr;
-	return newInt;
+		while (input != 0) {
+			node->next = new intNode;
+			node = node->next;
+			node->digit = input % 10;
+			input /= 10;
+		}
+		node->next = nullptr;
+		return newInt;
 
-}
-
-void printExtendedInt(const extendedInt intger) {
-	intNode * itr;
-	itr = intger;
-	while (itr != nullptr) {
-		cout << itr->digit;
-		itr = itr->next;
-	}
-	cout << endl;
-}
-
-void deleteInt(const extendedInt myInt) {
-	intNode * itr, *next;
-	itr = myInt;
-	while (itr != nullptr) {
-		next = itr->next;
-		delete itr;
-		itr = next;
-	}
-}
-
-extendedInt add(const extendedInt integer1, const extendedInt integer2) {
-	if (integer1 == nullptr || integer2 == nullptr) {
-		std::cerr << "empty pointer passed as argument";
-		return nullptr;
 	}
 
-	extendedInt newInteger = new intNode;
+	void printExtendedInt(const extendedInt intger) {
+		intNode * itr;
+		itr = intger;
+		while (itr != nullptr) {
+			cout << itr->digit;
+			itr = itr->next;
+		}
+		cout << endl;
+	}
 
-	intNode *itr1(integer1), *itr2(integer2), *itrNew(newInteger);
-	int carry = 0;
-	int digit1, digit2;
-	digit1 = itr1 ? itr1->digit : 0;
-	digit2 = itr2 ? itr2->digit : 0;
-	itrNew->digit = (digit1 + digit2 + carry) % 10;
-	carry = (digit1 + digit2 + carry) / 10;
-	
-	while (itr1->next != nullptr || itr2->next != nullptr || carry != 0) {
-		itr1 = itr1 ? itr1->next : nullptr;
-		itr2 = itr2 ? itr2->next : nullptr;
-		itrNew->next = new intNode;
-		itrNew = itrNew->next;
-		itrNew->next = nullptr;
+	void deleteInt( extendedInt & myInt) {
+		intNode * itr, *next;
+		itr = myInt;
+		while (itr != nullptr) {
+			next = itr->next;
+			delete itr;
+			itr = next;
+		}
+		myInt = nullptr;
+	}
 
+	extendedInt add(const extendedInt integer1, const extendedInt integer2) {
+		if (integer1 == nullptr || integer2 == nullptr) {
+			std::cerr << "error empty pointer passed as argument";
+			return nullptr;
+		}
+
+		extendedInt newInteger = new intNode;
+
+		//processing the head
+		intNode *itr1(integer1), *itr2(integer2), *itrNew(newInteger);
+		int carry = 0;
+		int digit1, digit2;
 
 		digit1 = itr1 ? itr1->digit : 0;
 		digit2 = itr2 ? itr2->digit : 0;
 		itrNew->digit = (digit1 + digit2 + carry) % 10;
 		carry = (digit1 + digit2 + carry) / 10;
-		cout << newInteger << endl;
+
+		itr1 = itr1 ? itr1->next : nullptr;
+		itr2 = itr2 ? itr2->next : nullptr;
+
+
+		//main part
+		while (itr1 != nullptr || itr2 != nullptr || carry != 0) {
+			itrNew->next = new intNode;
+			itrNew = itrNew->next;
+
+			digit1 = itr1 ? itr1->digit : 0;
+			digit2 = itr2 ? itr2->digit : 0;
+			itrNew->digit = (digit1 + digit2 + carry) % 10;
+			carry = (digit1 + digit2 + carry) / 10;
+			
+			itr1 = itr1 ? itr1->next : nullptr;
+			itr2 = itr2 ? itr2->next : nullptr;
+		}
+
+
+		itrNew->next = nullptr;
+
+		return newInteger;
+
 	}
 
-	cout <<  "asdasdasd" << endl;
-	printExtendedInt(newInteger);
-	itrNew->next = nullptr;
-	
-	return newInteger;
-	
-}
 
 
+int main() {
 
-int main(){
+	//test int to list
 	extendedInt myInt, myInt2;
 	myInt = intToList(255);
-	
-	
 	printExtendedInt(myInt);
-	cout << (myInt ? true : false) << endl;
-	myInt2 = intToList(1000);
+	
+	//test add
+	myInt2 = intToList(1855);
 	printExtendedInt(myInt2);
-
-	extendedInt myInt3 = add(myInt, myInt2);
+	extendedInt myInt3 = add(myInt, nullptr);
 	printExtendedInt(myInt3);
-	cout << "yabadaba";
+
+	deleteInt(myInt);
+	deleteInt(myInt2);
+	deleteInt(myInt3);
+
+	return 0;
 }
