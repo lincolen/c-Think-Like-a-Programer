c758e1f46daab01233a5461a918d242df6231bac
Eliram <eliram13@gmail.com>
2019-05-14 12:52:08 +0900

    wip extended int add

--- chapter 4 pointers/integerAsList.cpp
+++ chapter 4 pointers/integerAsList.cpp
@@ -59,9 +59,58 @@ void deleteInt(const extendedInt myInt) {
 	}
 }
 
+extendedInt add(const extendedInt integer1, const extendedInt integer2) {
+	if (integer1 == nullptr || integer2 == nullptr) {
+		std::cerr << "empty pointer passed as argument";
+		return nullptr;
+	}
+
+	extendedInt newInteger = new intNode;
+
+	intNode *itr1(integer1), *itr2(integer2), *itrNew(newInteger);
+	int carry = 0;
+	int digit1, digit2;
+	digit1 = itr1 ? itr1->digit : 0;
+	digit2 = itr2 ? itr2->digit : 0;
+	itrNew->digit = (digit1 + digit2 + carry) % 10;
+	carry = (digit1 + digit2 + carry) / 10;
+	
+	while (itr1->next != nullptr || itr2->next != nullptr || carry != 0) {
+		itr1 = itr1 ? itr1->next : nullptr;
+		itr2 = itr2 ? itr2->next : nullptr;
+		itrNew->next = new intNode;
+		itrNew = itrNew->next;
+		itrNew->next = nullptr;
+
+
+		digit1 = itr1 ? itr1->digit : 0;
+		digit2 = itr2 ? itr2->digit : 0;
+		itrNew->digit = (digit1 + digit2 + carry) % 10;
+		carry = (digit1 + digit2 + carry) / 10;
+		cout << newInteger << endl;
+	}
+
+	cout <<  "asdasdasd" << endl;
+	printExtendedInt(newInteger);
+	itrNew->next = nullptr;
+	
+	return newInteger;
+	
+}
+
+
+
 int main(){
-	extendedInt myInt;
+	extendedInt myInt, myInt2;
 	myInt = intToList(255);
-	cout << "hello" << endl;
+	
+	
 	printExtendedInt(myInt);
+	cout << (myInt ? true : false) << endl;
+	myInt2 = intToList(1000);
+	printExtendedInt(myInt2);
+
+	extendedInt myInt3 = add(myInt, myInt2);
+	printExtendedInt(myInt3);
+	cout << "yabadaba";
 }
