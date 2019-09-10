c1d3714edac01c6248f3fecca52b2b691fa8d92f
Eliram <eliram13@gmail.com>
2019-09-10 12:29:38 +0900

    Add word list funcunality

--- chapter 8/myWordList.cpp
+++ chapter 8/myWordList.cpp
@@ -5,6 +5,7 @@
 #include<fstream> 
 #include<assert.h> //assert 
 #include<algorithm> //foreach)()
+#include<cassert>
 
 using std::cout;
 using std::endl;
@@ -15,6 +16,8 @@ class MyWordList {
 		MyWordList(const std::string fileName); //construct from file
 		MyWordList & getWordsFromFile(const std::string fileName);
 		void printList() const;
+		int countWordsWithoutLetter(const char letter) const;
+		MyWordList & removeWordOfWrongLength(const int length);
 
 	private:
 		std::list<std::string> wordList;
@@ -22,7 +25,13 @@ class MyWordList {
 
 int main() {
 	MyWordList wordList("words.txt");
-	wordList.printList();
+
+	cout << "\nlength of list " << wordList.countWordsWithoutLetter('7') << endl;
+	cout << "number of words without the letter 'a' " << wordList.countWordsWithoutLetter('a') << endl;
+
+	cout << "\nmaintain only words that have three letters " << endl;
+	wordList.removeWordOfWrongLength(3).printList();
+	cout << "\nlength of list " << wordList.countWordsWithoutLetter('7') << endl;
 }
 
 MyWordList::MyWordList(const std::string fileName) {
@@ -48,5 +57,24 @@ MyWordList & MyWordList::getWordsFromFile(const std::string fileName) {
 
 void MyWordList::printList() const {
 	cout << "number of words in list: " << wordList.size() << endl;
-	std::for_each(this->wordList.begin(), this->wordList.end(), [](std::string str) {cout << str << endl;});
+	std::for_each(this->wordList.begin(), this->wordList.end(), [](const std::string & str) {cout << str << endl;});
+}
+
+int MyWordList::countWordsWithoutLetter(const char letter) const {
+	return std::count_if(this->wordList.begin(), this->wordList.end(), [letter](const std::string & str) {return str.find(letter) == std::string::npos;});
+}
+
+//remove all words from the list that have a diffrent number of letters then indicated by the argument
+MyWordList & MyWordList::removeWordOfWrongLength(const int length) {
+	assert("argument is non negtive" && length >= 0);
+	std::list<std::string>::iterator itr = this->wordList.begin(); 
+	while(itr != this->wordList.end()) {
+		if (itr->length() != length) {
+			itr = this->wordList.erase(itr);
+		}
+		else {
+			++itr;
+		}
+	}
+	return *this;
 }
